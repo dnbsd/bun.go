255a38117d5f4c47e8548bf3f9639edb22f4fdd4
dan <dnbsd@posteo.net>
2024-03-11 21:51:13 +0100

    Copy documentation from NATS

--- bun.go
+++ bun.go
@@ -8,25 +8,59 @@ import (
 )
 
 type Arguments struct {
-	Name                 string
-	Servers              []string
-	NoRandomize          bool
-	MaxReconnect         int
-	ReconnectWait        time.Duration
-	Timeout              time.Duration
-	DrainTimeout         time.Duration
-	FlusherTimeout       time.Duration
-	PingInterval         time.Duration
-	MaxPingsOut          int
-	User                 string
-	Password             string
-	Token                string
-	Compression          bool
+	// Name is an optional name label which will be sent to the server on CONNECT to identify the client.
+	Name string
+	// Servers is a configured set of servers which this client will use when attempting to connect.
+	Servers []string
+	// NoRandomize configures whether we will randomize the server pool.
+	NoRandomize bool
+	// MaxReconnect sets the number of reconnect attempts that will be tried before giving up. If negative,
+	// then it will never give up trying to reconnect.
+	// Defaults to 60.
+	MaxReconnect int
+	// ReconnectWait sets the time to backoff after attempting a reconnect to a server that we were already
+	// connected to previously.
+	// Defaults to 2s.
+	ReconnectWait time.Duration
+	// Timeout sets the timeout for a Dial operation on a connection.
+	// Defaults to 2s.
+	Timeout time.Duration
+	// DrainTimeout sets the timeout for a Drain Operation to complete.
+	// Defaults to 30s.
+	DrainTimeout time.Duration
+	// FlusherTimeout is the maximum time to wait for write operations to the underlying connection to complete
+	// (including the flusher loop).
+	// Defaults to 1m.
+	FlusherTimeout time.Duration
+	// PingInterval is the period at which the client will be sending ping commands to the server disabled if 0 or
+	// negative.
+	// Defaults to 2m.
+	PingInterval time.Duration
+	// MaxPingsOut is the maximum number of pending ping commands that can be awaiting a response before raising
+	// an ErrStaleConnection error.
+	// Defaults to 2.
+	MaxPingsOut int
+	// User sets the username to be used when connecting to the server.
+	User string
+	// Password sets the password to be used when connecting to a server.
+	Password string
+	// Token sets the token to be used when connecting to a server.
+	Token string
+	// For websocket connections, indicates to the server that the connection
+	// supports compression. If the server does too, then data will be compressed.
+	Compression bool
+	// IgnoreAuthErrorAbort - if set to true, client opts out of the default connect behavior of aborting
+	// subsequent reconnect attempts if server returns the same auth error twice (regardless of reconnect policy).
 	IgnoreAuthErrorAbort bool
-	SkipHostLookup       bool
-	MsgChanCapacity      int
-	UserJWTHandler       UserJWTHandlerFunc
-	SignatureHandler     SignatureHandlerFunc
+	// SkipHostLookup skips the DNS lookup for the server hostname.
+	SkipHostLookup bool
+	// MsgChanCapacity is the size of the buffered channel used for ChanQueueSubscribe.
+	// Defaults to 65536.
+	MsgChanCapacity int
+	// UserJWT sets the callback handler that will fetch a user's JWT.
+	UserJWTHandler UserJWTHandlerFunc
+	// SignatureCB designates the function used to sign the nonce presented from the server.
+	SignatureHandler SignatureHandlerFunc
 }
 
 type subscribe struct {
